3a2ca92ce8d889cf5c25146b9ae6e72d0a1ee196
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2018-11-30 19:58:13 -0300

    Correcoes de bugs no ponto

--- docs/scripts/controller/CalcController.js
+++ docs/scripts/controller/CalcController.js
@@ -133,7 +133,7 @@ class CalcController {
 			} else {
 				//transforma o numero anterior e o digitado em string e concatena
 				let newValue = this.getLastOperation().toString() + value.toString();
-				this.setLastOperation(parseFloat(newValue));
+				this.setLastOperation(newValue);
 
 				this.setLastNumberToDisplay();
 			}
@@ -169,6 +169,8 @@ class CalcController {
 
 	addDot(){
 		let lastOperation = this.getLastOperation();
+
+		if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
 		if(this.isOperator(lastOperation) || !lastOperation){
 			this.pushOperation('0.');
